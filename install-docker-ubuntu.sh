107b769d2ca76a457cee1f95029e46e7ebb3d7b1
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-05-11 20:48:32 +0300

    Changes for run scheduler

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -7,9 +7,6 @@ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
-apt-get install -y docker-ce python python-pip
-pip install --upgrade pip
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # here usermod -aG docker username
