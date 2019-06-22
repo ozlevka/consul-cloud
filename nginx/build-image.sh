9d76fb558a505bb3d62c1f6d813ceb4a80527178
ozlevka <ozlevka@gmail.com>
2019-06-22 09:12:12 +0300

    Clear branch

--- nginx/build-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -v $(pwd):/app ozlevka/passmaker:4
-
-docker build -t nginx:current .
--- nginx/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp /server.conf /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
